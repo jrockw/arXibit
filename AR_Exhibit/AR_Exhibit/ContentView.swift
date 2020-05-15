5e14ec590d3d3e86159a686a587d26ea2d528ccd
jrockw <jrockw@umich.edu>
2020-05-14 17:57:00 -0700

    project

---
+++ AR_Exhibit/AR_Exhibit/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  AR_Exhibit
+//
+//  Created by Jacob Rockwell on 5/14/20.
+//  Copyright © 2020 Jacob Rockwell. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
