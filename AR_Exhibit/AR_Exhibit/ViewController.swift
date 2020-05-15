22de9440b198cdccff6eb03bbff7a4907b924ddb
jrockw <jrockw@umich.edu>
2020-05-15 11:49:51 -0700

    Here ya go

--- AR_Exhibit/AR_Exhibit/ViewController.swift
+++ AR_Exhibit/AR_Exhibit/ViewController.swift
@@ -18,12 +18,35 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
    
         setupARView()
+        
+         arView.addGestureRecognizer(UITapGestureRecognizer(target:self, action: #selector(handleTap(recognizer:))))
+        
+        
     }
+    
+   
     
     // MARK: Setup Methods This is what I can't get to work for ze life of me. it doesnt know what automatically configure session is.
     
     func setupARView () {
         arView.automaticallyConfigureSession = false
-        let configuration = ARWorldTrackingConfiguration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+    }
+    @objc
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: arView)
+        
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstResult = results.first {
+            // I think this is where he's specifying the input 3D image
+            let anchor = ARAnchor(name: "ContemporaryFan", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else {
+            print("ERROR: Object placement failed to find surface.")
+        }
     }
 }
